43c842f2f0722bd1b7ddf021ba83bb48256c0d46
Paul Roberts <proberts444@gmail.com>
2024-10-16 15:24:45 -0600

    adding more

--- utils/util.js
+++ utils/util.js
@@ -6,6 +6,7 @@
  * @returns
  */
 export function urlBase64ToUint8Array(base64String) {
+    console.log('called urlBase64ToUint8Array() function', base64String)
     const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
     const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
 
