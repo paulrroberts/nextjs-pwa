3965f3b148abf4c3f388c32f87315c79bc5355cf
Paul Roberts <proberts444@gmail.com>
2024-10-16 14:59:01 -0600

    fixing bad links

--- app/layout.jsx
+++ app/layout.jsx
@@ -35,20 +35,20 @@ export default function RootLayout({ children }) {
             <meta name="description" content="Best PWA App in the world" />
             <meta name="format-detection" content="telephone=no" />
             <meta name="mobile-web-app-capable" content="yes" />
-            <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+            {/* <meta name="msapplication-config" content="/icons/browserconfig.xml" /> */}
             <meta name="msapplication-TileColor" content="#2B5797" />
             <meta name="msapplication-tap-highlight" content="no" />
             <meta name="theme-color" content="#000000" />
 
-            <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+            {/* <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
             <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
             <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
-            <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
+            <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" /> */}
 
-            <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+            {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" /> */}
             <link rel="manifest" href="/manifest.json" />
-            <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
             <link rel="shortcut icon" href="/favicon.ico" />
             <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
 
@@ -63,7 +63,7 @@ export default function RootLayout({ children }) {
             <meta property="og:description" content="Best PWA App in the world" />
             <meta property="og:site_name" content="PWA App" />
             <meta property="og:url" content="https://nextjs-pwa-git-main-paulrroberts-projects.vercel.app/" />
-            <meta property="og:image" content="https://nextjs-pwa-git-main-paulrroberts-projects.vercel.app/icons/apple-touch-icon.png" />
+            <meta property="og:image" content="https://nextjs-pwa-git-main-paulrroberts-projects.vercel.app/apple-touch-icon.png" />
             {/* </Head> */}
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
                 <Card sx={{ height: '100vh' }}>
