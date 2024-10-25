80418a1d1da2db8fee4bcd14e1e9ffa93a80922b
Paul Roberts <proberts444@gmail.com>
2024-10-25 15:42:59 -0600

    trying build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
     dest: 'public',
-    sw: '/sw.js'
+    sw: '/sw.js',
+    register: true,
+    skipWaiting: true
 })
 
 module.exports = withPWA()
