b9e40318a42098950a468c615b30a7522a564991
Paul Roberts <proberts444@gmail.com>
2024-10-25 10:55:59 -0600

    test install button

--- app/install/page.jsx
+++ app/install/page.jsx
@@ -1,9 +1,11 @@
 import PushNotificationManager from '../../components/pushNotificationManager'
+import InstallPrompt from '../../components/installPrompt'
 
 export default function Favorites() {
     return (
         <div>
             <PushNotificationManager />
+            <InstallPrompt />
         </div>
     )
 }
