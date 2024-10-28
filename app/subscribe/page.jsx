4bb83c8b8a4a89ae32bd3f144a2f56e37c1ed775
Paul Roberts <proberts444@gmail.com>
2024-10-28 15:39:15 -0600

    Don't need this anymore

--- app/subscribe/page.jsx
+++ app/subscribe/page.jsx
@@ -1,11 +1,9 @@
 import PushNotificationManager from '../../components/pushNotificationManager'
-import InstallPrompt from '../../components/installPrompt'
 
 export default function Favorites() {
     return (
         <div>
             <PushNotificationManager />
-            <InstallPrompt />
         </div>
     )
 }
