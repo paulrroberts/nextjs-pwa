132ab4e3a739e40b1d0ccf76798f450b8af787bd
Paul Roberts <proberts444@gmail.com>
2024-10-28 14:09:03 -0600

    set favicon stuff

--- app/layout.jsx
+++ app/layout.jsx
@@ -43,24 +43,13 @@ export default function RootLayout({ children }) {
                 <meta name="msapplication-tap-highlight" content="no" />
                 <meta name="theme-color" content="#000000" />
 
-                {/* <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-            <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
-            <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
-            <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" /> */}
-
-                {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" /> */}
-                <link rel="manifest" href="/manifest.json" />
-                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
                 <link rel="shortcut icon" href="/favicon.ico" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <meta name="apple-mobile-web-app-title" content="Nu Skin" />
+                <link rel="manifest" href="/site.webmanifest" />
 
-                {/* <meta name="twitter:card" content="summary" />
-                <meta name="twitter:url" content="https://yourdomain.com" />
-                <meta name="twitter:title" content="PWA App" />
-                <meta name="twitter:description" content="Best PWA App in the world" />
-                <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
-                <meta name="twitter:creator" content="@DavidWShadow" /> */}
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content="PWA App" />
                 <meta property="og:description" content="Best PWA App in the world" />
