d7e34a6ac1008465d6f635efad54a8769b98cdd5
Paul Roberts <proberts444@gmail.com>
2024-10-28 09:34:27 -0600

    Fix: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
+const isDev = process.env.NODE_ENV !== 'production'
+
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
     dest: 'public',
     sw: '/sw.js',
     register: true,
-    skipWaiting: true
+    skipWaiting: true,
+    exclude: [
+        // add buildExcludes here
+        ({ asset, compilation }) => {
+            if (asset.name.startsWith('server/') || asset.name.match(/^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/)) {
+                return true
+            }
+            if (isDev && !asset.name.startsWith('static/runtime/')) {
+                return true
+            }
+            return false
+        }
+    ]
 })
 
 module.exports = withPWA()
