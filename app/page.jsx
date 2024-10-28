743edfba5b5f326e4f138e77eed5c230cae27ffe
Paul Roberts <proberts444@gmail.com>
2024-10-28 14:06:04 -0600

    update images

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,9 @@
-import PushNotificationManager from '../components/pushNotificationManager'
-import InstallPrompt from '../components/installPrompt'
 import HomeContent from '../components/home/homeContent'
 
 export default function Page() {
     return (
         <div>
             <HomeContent />
-            {/* <PushNotificationManager />
-            <InstallPrompt /> */}
         </div>
     )
 }
