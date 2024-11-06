47d734b1097c2917081d4c72b5d40fa3001fd04e
Paul Roberts <proberts444@gmail.com>
2024-11-06 14:44:21 -0700

    fix show install

--- components/addToHomeScreen/addToHomeScreen.jsx
+++ components/addToHomeScreen/addToHomeScreen.jsx
@@ -24,7 +24,7 @@ export default function AddToHomeScreen({ checkcookie, callback }) {
 
     const closePrompt = () => {
         setDisplayPrompt('')
-        callback(false)
+        callback(true)
     }
 
     const doNotShowAgain = () => {
