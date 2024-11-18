68bc37986552bbf053c4f2ab1d449fc9142d1bc8
Paul Roberts <proberts444@gmail.com>
2024-11-18 14:10:33 -0700

    trying to fix this

--- app/api/api-test/route.js
+++ app/api/api-test/route.js
@@ -1,3 +1,5 @@
+import { NextResponse } from 'next/server'
+
 export async function GET(request) {
     return new Response(JSON.stringify({ message: "You've reached the GET endpoint for the PWA POC" }), {
         status: 200,
@@ -6,10 +8,16 @@ export async function GET(request) {
 }
 
 export async function POST(req, res) {
-    const body = await req.json()
-    await res.revalidate('/nuskin/isr')
-    return new Response(JSON.stringify({ receivedData: body }), {
-        status: 201,
-        headers: { 'Content-Type': 'application/json' }
-    })
+    try {
+        // const { path } = await request.json() // Get the path to revalidate from the POST body.
+        const path = '/nuskin/isr'
+
+        // Trigger revalidation for the given path.
+        await res.revalidate(path)
+
+        return NextResponse.json({ message: `Revalidation triggered for ${path}` }, { status: 200 })
+    } catch (error) {
+        console.error('Error during revalidation:', error)
+        return NextResponse.json({ error: 'Failed to revalidate' }, { status: 500 })
+    }
 }
