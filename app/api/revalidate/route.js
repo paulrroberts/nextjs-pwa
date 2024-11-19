d87272300244e692149e31adcee36b80516d2bfe
Paul Roberts <proberts444@gmail.com>
2024-11-19 13:50:21 -0700

    use fallback path to revalidate

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -1,15 +1,16 @@
 // app/api/revalidate/route.js
 import { NextResponse } from 'next/server'
 import { revalidatePath } from 'next/cache'
+import { isNull } from '@nuskin/uncle-buck'
 
-export async function POST(request, res) {
+export async function POST(request) {
     try {
-        const { path } = await request.json() // Get the path to revalidate from the POST body.
+        let { path } = await request.json() // Get the path to revalidate from the POST body.
+        if (isNull(path)) {
+            path = '/nuskin/isr'
+        }
+
         revalidatePath(path)
-
-        // Trigger revalidation for the given path.
-        // await res.revalidate(path)
-
         return NextResponse.json({ message: `Revalidation triggered for ${path}` }, { status: 200 })
     } catch (error) {
         console.error('Error during revalidation:', error)
