6331a6771a5e6d6f4ba7fb320f84ea254112cac3
Paul Roberts <proberts444@gmail.com>
2024-11-19 13:40:04 -0700

    pull path from request

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -4,13 +4,13 @@ import { revalidatePath } from 'next/cache'
 
 export async function POST(request, res) {
     try {
-        revalidatePath('/nuskin/isr')
-        // const { path } = await request.json() // Get the path to revalidate from the POST body.
+        const { path } = await request.json() // Get the path to revalidate from the POST body.
+        revalidatePath(path)
 
         // Trigger revalidation for the given path.
         // await res.revalidate(path)
 
-        // return NextResponse.json({ message: `Revalidation triggered for ${path}` }, { status: 200 })
+        return NextResponse.json({ message: `Revalidation triggered for ${path}` }, { status: 200 })
     } catch (error) {
         console.error('Error during revalidation:', error)
         return NextResponse.json({ error: 'Failed to revalidate' }, { status: 500 })
