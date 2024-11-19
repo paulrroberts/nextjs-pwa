79ed9164089f87b3baf587257e3df3849b217179
Paul Roberts <proberts444@gmail.com>
2024-11-19 13:34:23 -0700

    trying a different approach

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -1,14 +1,16 @@
 // app/api/revalidate/route.js
 import { NextResponse } from 'next/server'
+import { revalidatePath } from 'next/cache'
 
 export async function POST(request, res) {
     try {
-        const { path } = await request.json() // Get the path to revalidate from the POST body.
+        revalidatePath('/nuskin/isr')
+        // const { path } = await request.json() // Get the path to revalidate from the POST body.
 
         // Trigger revalidation for the given path.
-        await res.revalidate(path)
+        // await res.revalidate(path)
 
-        return NextResponse.json({ message: `Revalidation triggered for ${path}` }, { status: 200 })
+        // return NextResponse.json({ message: `Revalidation triggered for ${path}` }, { status: 200 })
     } catch (error) {
         console.error('Error during revalidation:', error)
         return NextResponse.json({ error: 'Failed to revalidate' }, { status: 500 })
